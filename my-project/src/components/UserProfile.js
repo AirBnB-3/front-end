82cc0c550ae338e0c9bf18c2a0d2c4056df83122
Sigi Jaeckel <sigij55@gmail.com>
2020-06-23 15:10:42 -0400

    create listing component

---
+++ my-project/src/components/UserProfile.js
@@ -0,0 +1,12 @@
+import React, {Link} from 'react'
+
+
+export default function UserProfile(props){
+
+
+    return(
+        <div>
+         <Link className='link' to='/createlisting'>Create A Listing</Link>
+        </div>
+    )
+}
